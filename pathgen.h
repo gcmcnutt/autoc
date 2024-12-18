5578f7de888a4caee52a46952dc8b7607260f9f8
Greg McNutt <gcmcnutt@yahoo.com>
2024-12-17 21:14:59 -0800

    more canned paths

--- pathgen.h
+++ pathgen.h
@@ -154,11 +154,118 @@ class GenerateClassic : public GeneratorMethod {
   }
 };
 
+enum PathType {
+  HorizontalCircle = 0,
+  VerticalCircle,
+  FigureEight,
+  HorizontalSquare,
+  VerticalSquare,
+  FortyFiveLoop,
+  END_MARKER
+};
+
 class GenerateComputedPaths : public GeneratorMethod {
   std::vector<Path> method(int pathIndex, double radius, double height, double base) override {
     std::vector<Path> path;
 
-    assert(false);
+    PathType pathType = static_cast<PathType>(pathIndex % static_cast<int>(PathType::END_MARKER));
+
+    switch(pathType) {
+      case PathType::HorizontalCircle: {
+        double cSize = 0.8 * radius;
+        for (double turn = 0; turn < M_PI * 2; turn += 0.05) {
+          Eigen::Vector3d interpolatedPoint = { cSize * cos(turn), cSize * sin(turn), base - SIM_INITIAL_LOCATION_DITHER };
+          Path pathSegment = { interpolatedPoint, Eigen::Vector3d::UnitX(), turn * radius, 0.0 };
+          path.push_back(pathSegment);
+        }
+        break;
+      }
+      case PathType::VerticalCircle: {
+        double cSize = 0.8 * radius;
+        for (double turn = 0; turn < M_PI * 2; turn += 0.05) {
+          Eigen::Vector3d interpolatedPoint = { cSize * cos(turn), 0.0, base - SIM_INITIAL_LOCATION_DITHER - cSize * sin(turn)};
+          Path pathSegment = { interpolatedPoint, Eigen::Vector3d::UnitX(), turn * cSize, 0.0 };
+          path.push_back(pathSegment);
+        }
+        break;
+      }
+      case PathType::FigureEight: {
+        double cSize = 0.4 * radius;
+        for (double turn = 0; turn < M_PI * 2; turn += 0.05) {
+          Eigen::Vector3d interpolatedPoint = { (cSize * cos(turn)) - cSize, 0.0, base - SIM_INITIAL_LOCATION_DITHER - (cSize * sin(turn))};
+          Path pathSegment = { interpolatedPoint, Eigen::Vector3d::UnitX(), (turn * cSize), 0.0 };
+          path.push_back(pathSegment);
+        }
+        for (double turn = 0; turn < M_PI * 2; turn += 0.05) {
+          Eigen::Vector3d interpolatedPoint = { (-cSize * cos(turn)) + cSize, 0.0, base - SIM_INITIAL_LOCATION_DITHER - (cSize * sin(turn))};
+          Path pathSegment = { interpolatedPoint, Eigen::Vector3d::UnitX(), (turn * cSize) + (M_PI * 2 * cSize), 0.0 };
+          path.push_back(pathSegment);
+        }
+        break;
+      }
+      case PathType::HorizontalSquare: {
+        double sSize = 0.7 * radius;
+        for (double span = -sSize; span < sSize; span += 0.1) {
+          Eigen::Vector3d interpolatedPoint = { span, -sSize, base - SIM_INITIAL_LOCATION_DITHER };
+          Path pathSegment = { interpolatedPoint, Eigen::Vector3d::UnitX(), span, 0.0 };
+          path.push_back(pathSegment);
+        }  
+        for (double span = -sSize; span < sSize; span += 0.1) {
+          Eigen::Vector3d interpolatedPoint = { sSize, span, base - SIM_INITIAL_LOCATION_DITHER };
+          Path pathSegment = { interpolatedPoint, Eigen::Vector3d::UnitX(), 2 * sSize + span, 0.0 };
+          path.push_back(pathSegment);
+        } 
+        for (double span = -sSize; span < sSize; span += 0.1) {
+          Eigen::Vector3d interpolatedPoint = { -span, sSize, base - SIM_INITIAL_LOCATION_DITHER };
+          Path pathSegment = { interpolatedPoint, Eigen::Vector3d::UnitX(), 4 * sSize + span, 0.0 };
+          path.push_back(pathSegment);
+        }  
+        for (double span = -sSize; span < sSize; span += 0.1) {
+          Eigen::Vector3d interpolatedPoint = { -sSize, -span, base - SIM_INITIAL_LOCATION_DITHER };
+          Path pathSegment = { interpolatedPoint, Eigen::Vector3d::UnitX(), 6 * sSize + span, 0.0 };
+          path.push_back(pathSegment);
+        }      
+        break;
+      }
+      case PathType::VerticalSquare: {
+        double sSize = 0.7 * radius;
+        for (double span = -sSize; span < sSize; span += 0.1) {
+          Eigen::Vector3d interpolatedPoint = { span, 0, base - SIM_INITIAL_LOCATION_DITHER - sSize };
+          Path pathSegment = { interpolatedPoint, Eigen::Vector3d::UnitX(), span, 0.0 };
+          path.push_back(pathSegment);
+        }  
+        for (double span = -sSize; span < sSize; span += 0.1) {
+          Eigen::Vector3d interpolatedPoint = { sSize, 0, base - SIM_INITIAL_LOCATION_DITHER + span };
+          Path pathSegment = { interpolatedPoint, Eigen::Vector3d::UnitX(), 2 * sSize + span, 0.0 };
+          path.push_back(pathSegment);
+        } 
+        for (double span = -sSize; span < sSize; span += 0.1) {
+          Eigen::Vector3d interpolatedPoint = { -span, 0, base - SIM_INITIAL_LOCATION_DITHER + sSize };
+          Path pathSegment = { interpolatedPoint, Eigen::Vector3d::UnitX(), 4 * sSize + span, 0.0 };
+          path.push_back(pathSegment);
+        }  
+        for (double span = -sSize; span < sSize; span += 0.1) {
+          Eigen::Vector3d interpolatedPoint = { -sSize, 0, base - SIM_INITIAL_LOCATION_DITHER -span };
+          Path pathSegment = { interpolatedPoint, Eigen::Vector3d::UnitX(), 6 * sSize + span, 0.0 };
+          path.push_back(pathSegment);
+        }      
+        break;
+      }
+      case PathType::FortyFiveLoop: {
+        double cSize = 0.8 * radius;
+        for (double turn = 0; turn < M_PI * 2; turn += 0.05) {
+          Eigen::Vector3d interpolatedPoint = { cSize * cos(turn), cSize * sin(turn), base - SIM_INITIAL_LOCATION_DITHER - cSize * sin(turn)};
+          Path pathSegment = { interpolatedPoint, Eigen::Vector3d::UnitX(), turn * cSize, 0.0 };
+          path.push_back(pathSegment);
+        }
+        break;
+      }
+      default: {
+        std::cerr << "Unknown path type: " << pathType << std::endl;
+        assert(false);
+      }
+    }
+
     return path;
   }
 };
